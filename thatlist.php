a5235fe1bbee6b03b9795a3bd91a71729cf81436
Dev Ulla <56549396+dehla@users.noreply.github.com>
2021-10-31 04:08:31 +0700

    Create thatlist.php
    adding function 1

---
+++ thatlist.php
@@ -0,0 +1,13 @@
+<?php
+
+function get(){
+	return trim(fgets(STDIN));
+}
+
+function get_between($string, $start, $end) 
+    {
+        $string = " ".$string;
+        $ini = strpos($string,$start);
+        if ($ini == 0) return "";
+  
+  ?>
