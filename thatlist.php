737f08a5bf05e8af0d5a0c5f296697ba26303f2b
Dev Ulla <56549396+dehla@users.noreply.github.com>
2021-10-31 04:31:19 +0700

    updating thatlist.php | funct1
    adding Function during session

--- thatlist.php
+++ thatlist.php
@@ -9,5 +9,9 @@ function get_between($string, $start, $end)
         $string = " ".$string;
         $ini = strpos($string,$start);
         if ($ini == 0) return "";
-  
+  	$ini += strlen($start);
+        $len = strpos($string,$end,$ini) - $ini;
+        return substr($string,$ini,$len);
+    }
+
   ?>
